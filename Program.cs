5d75fbf5a938255cf5048cf10176ee2769a4b541
coolcoder75 <mdragon.shock@gmail.com>
2022-07-02 21:36:18 +1000

    2

--- Program.cs
+++ Program.cs
@@ -1,57 +1,41 @@
-﻿using System;
-
-namespace ConsoleApp1
+﻿static int FuncFive(int[] input)
 {
-    class Program
+
+    int my = 0;
+
+    int total = input.Length;
+
+    int index = 0;
+
+    int[,] ar1 = new int[1, total];
+
+    int[,] ar2 = new int[total, total];
+
+    for (int i = 0; i < 1; i++)
     {
-        static int FuncFive(int[] input)
+        for (int j = 0; j < total; j++)
         {
 
-            int my = 0;
+            ar1[i, j] = input[index++];
 
-            int total = input.Length;
-
-            int index = 0;
-
-            int[,] ar1 = new int[1, total];
-
-            int[,] ar2 = new int[total, total];
-
-            for (int i = 0; i < 1; i++)
+            for (int z = 0; z < total; z++)
             {
-                for (int j = 0; j < total; j++)
-                {
 
-                    ar1[i, j] = input[index++];
+                ar2[i, j] = ar1[i, j] * input[z];
 
-                    for (int z = 0; z < total; z++)
-                    {
-
-                        ar2[i, j] = ar1[i, j] * input[z];
-
-                        Console.Write(ar2[i, j] + " ");
-
-                    }
-                    Console.WriteLine();
-
-
-                }
+                Console.Write(ar2[i, j] + " ");
 
             }
+            Console.WriteLine();
 
 
-            return my;
         }
 
-     
-
-        static void Main(string[] args)
-        {
-
-            int[] ArrayF5 = { 1, 2, 3, 4, 5 };
-
-            FuncFive(ArrayF5);
-
-        }
     }
+
+
+    return my;
 }
+
+int[] ArrayF5 = { 1, 2, 3, 4, 5 };
+FuncFive(ArrayF5);
